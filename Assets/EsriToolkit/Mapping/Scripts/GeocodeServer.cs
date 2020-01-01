6173e687dc1e3dcb257ac3b215f11fa9e263a386
Telemething <mark@telemething.com>
2020-01-01 14:14:57 -0800

    Imported ArcGIS, commented out HTK gaze

---
+++ Assets/EsriToolkit/Mapping/Scripts/GeocodeServer.cs
@@ -0,0 +1,88 @@
+﻿/*
+    Copyright 2016 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+using SimpleJSON;
+using System;
+using System.Collections;
+//using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Esri.PrototypeLab.HoloLens.Unity {
+    public static class GeocodeServer {
+        private const string URL = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+        public static IEnumerator ReverseGeocode(Coordinate coordinate, Action<Address> callback) {
+            string url = string.Format("{0}/reverseGeocode?location={1},{2}&f=json", new object[] {
+                URL,
+                coordinate.Longitude,
+                coordinate.Latitude
+            });
+            WWW www = new WWW(url);
+            yield return www;
+
+            // Extract token from parsed response
+            var text = www.text.Replace(":null", ":\"\"");
+            var json = JSON.Parse(text);
+            if (json.ContainsKey("error")) {
+                callback(null);
+                yield break;
+            }
+            var address = json["address"];
+            callback(new Address() {
+                Street = address["Address"].Value,
+                City = address["City"].Value,
+                Region = address["Region"].Value,
+                Postal = address["Postal"].Value,
+                Country = address["CountryCode"].Value,
+                SingleLine = address["Match_addr"].Value
+            });
+        }
+        //public static IEnumerator ReverseGeocodeRT(Coordinate coordinate, Action<Address> callback) {
+        //    // Create task
+        //    var task = Task.Run(async () => {
+        //        // Perform Esri Runtime reverse geocoding asynchronously.
+        //        LocatorTask locatorTask = new LocatorTask(new Uri(URL));
+        //        await locatorTask.LoadAsync();
+        //        MapPoint point = new MapPoint(
+        //            coordinate.Longitude,
+        //            coordinate.Latitude,
+        //            new SpatialReference(4326)
+        //        );
+        //        ReverseGeocodeParameters parameters = new ReverseGeocodeParameters() {
+        //            MaxResults = 1
+        //        };
+        //        var results = await locatorTask.ReverseGeocodeAsync(point, parameters);
+        //        if (results.Count == 0 || results[0] == null) { return null; }
+        //        return results[0].Attributes;
+        //    });
+
+        //    // Wait for Esri reverse geocoding to complete.
+        //    yield return new WaitUntil(() => task.IsCompleted);
+
+        //    // No address found.
+        //    if (task.Result == null) { yield break; }
+
+        //    // Return address as structured object.
+        //    callback(new Address() {
+        //        Street = task.Result["Address"].ToString(),
+        //        City = task.Result["City"].ToString(),
+        //        Region = task.Result["Region"].ToString(),
+        //        Postal = task.Result["Postal"].ToString(),
+        //        Country = task.Result["CountryCode"].ToString(),
+        //        SingleLine = task.Result["Match_addr"].ToString()
+        //    });
+        //}
+    }
+}
---
+++ Assets/EsriToolkit/Mapping/Scripts/Place.cs
@@ -0,0 +1,101 @@
+﻿/*
+    Copyright 2016 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+namespace Esri.PrototypeLab.HoloLens.Unity {
+    public class Place {
+        public string Name { get; set; }
+        public Coordinate Location { get; set; }
+        public int Level { get; set; }
+        public static Place[] PresetPlaces {
+            get {
+                return new Place[] {
+                    new Place() {
+                        Name = "hoover dam",
+                        Location = new Coordinate() {
+                            Longitude = -114.737778f,
+                            Latitude = 36.015556f
+                        },
+                        Level = 12
+                    },
+                    new Place() {
+                        Name = "mount everest",
+                        Location = new Coordinate() {
+                            Longitude = 86.925278f,
+                            Latitude = 27.988056f
+                        },
+                        Level = 10
+                    },
+                    new Place() {
+                        Name = "mount saint helens",
+                        Location = new Coordinate() {
+                            Longitude = -122.1944f,
+                            Latitude = 46.1912f
+                        },
+                        Level = 10
+                    },
+                    new Place() {
+                        Name = "paris",
+                        Location = new Coordinate() {
+                            Longitude = 2.3508f,
+                            Latitude = 48.8567f
+                        },
+                        Level = 9
+                    },
+                    new Place() {
+                        Name = "los angeles",
+                        Location = new Coordinate() {
+                            Longitude = -118.25f,
+                            Latitude = 34.05f
+                        },
+                        Level = 9
+                    },
+                    new Place() {
+                        Name = "machu picchu",
+                        Location = new Coordinate() {
+                            Longitude = -72.545556f,
+                            Latitude = -13.163333f
+                        },
+                        Level = 10
+                    },
+                    new Place() {
+                        Name = "mount taranaki",
+                        Location = new Coordinate() {
+                            Longitude = 174.064722f,
+                            Latitude = -39.296389f
+                        },
+                        Level = 11
+                    },
+                    new Place() {
+                        Name = "mount maunganui",
+                        Location = new Coordinate() {
+                            Longitude = 176.185f,
+                            Latitude = -37.643f
+                        },
+                        Level = 12
+                    },
+                    new Place() {
+                        Name = "wellington",
+                        Location = new Coordinate() {
+                            Longitude = 174.777222f,
+                            Latitude = -41.288889f
+                        },
+                        Level = 12
+                    }
+                };
+            }
+        }
+    }
+}
